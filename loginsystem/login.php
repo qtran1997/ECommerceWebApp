d043dac5d0c2301214068a213da0f8dde486142a
Quang <qtran1997@gmail.com>
2018-05-16 12:20:40 -0700

    v2.01 (Added Login System)

---
+++ loginsystem/login.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+    include 'dbh.php';
+
+    $uid = $_POST['uid'];
+    $pwd = $_POST['pwd'];
+    $salt = "How1Did12You123Get1234Here!!@#420XD102_";
+
+    $pwd = hash('sha256', $salt . $pwd); 
+
+    $sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
+
+    $result = mysqli_query($conn, $sql);
+    
+    if(!$row = mysqli_fetch_assoc($result))
+    {
+        header("Location: ../loginpage.php?failure=1");
+        
+    }
+    else
+    {
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['uid'] = $row['uid'];
+        $_SESSION['email'] = $row['email'];
+    
+        header("Location: ../accountpage.php");
+    }
+    
+    
+?>
---
+++ loginsystem/signup.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();
+    include 'dbh.php';
+
+    $email = $_POST['email'];
+    $uid = $_POST['uid'];
+    $pwd = $_POST['pwd'];
+
+//    echo $email . "<br>";
+//    echo $uid . "<br>";
+//    echo $pwd . "<br>";
+
+    $salt = "How1Did12You123Get1234Here!!@#420XD102_";
+    $pwd = hash('sha256', $salt . $pwd); 
+    
+
+    $sql = "SELECT * FROM user WHERE uid='$uid' OR email='$email'";
+
+    $result = mysqli_query($conn, $sql);
+
+    if(!$row = mysqli_fetch_assoc($result))
+    {
+        $sql = "INSERT INTO user (email, uid, pwd) 
+        VALUES ('$email', '$uid', '$pwd')";
+        $result = mysqli_query($conn, $sql);
+        
+
+        $_SESSION['id'] = $row['id'];
+        header("Location: ../index.php");
+    }
+    else
+    {
+        header("Location: ../signuppage.php?failure=1");
+    }
+?>
