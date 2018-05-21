5e635d95812d880b20a969fcf4d9e178bb158f0e
Quang Tran <qtran1997@gmail.com>
2018-05-21 16:14:39 -0700

    full push

---
+++ loginsystem/signupsubmit.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+    include 'dbh.php';
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $pwd1 = $_POST['pwd1'];
+    $pwd2 = $_POST['pwd2'];
+
+
+    if($pwd1 != $pwd2)
+    {
+        header("Location: ../signup.php?failureP=1");
+    }
+    else
+    {
+        $pwd = $pwd1;
+    }
+
+    
+    
+    $salt = "How1Did12You123Get1234Here!!@#420XD102_";
+    $pwd = hash('sha256', $salt . $pwd); 
+    
+
+    $sql = "SELECT * FROM aclickerlogin WHERE email='$email' ";
+
+    
+
+    $result = mysqli_query($conn, $sql);
+
+    if (!$result) {
+        echo 'MySQL Error: ' . mysqli_error($conn);
+        exit;
+    }
+
+    if(!$row = mysqli_fetch_assoc($result))
+    {
+        $sql = "INSERT INTO aclickerlogin (fname, lname, email, pwd) 
+        VALUES ('$fname','$lname','$email', '$pwd')";
+        $result = mysqli_query($conn, $sql);
+        
+
+        $_SESSION['id'] = $row['id'];
+        echo "hello";
+        header("Location: ../index.php");
+    }
+    else
+    {
+        header("Location: ../signup.php?failureE=1");
+    }
+?>
---
+++ navloggedin.php
@@ -0,0 +1,70 @@
+<!--
+<div class="responsive-bar">
+<div class="logo">
+<a href="#"><img src="Images/sheeklogo.jpg" alt="logo"></a>
+</div>
+<div class="menu">
+<h4>Menu</h4>
+</div>
+</div>
+-->
+
+<!--
+<div class="jumbotron" style="margin-bottom: 0px;">
+
+</div>
+-->
+
+<nav class="navbar navbar-expand-md">
+
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapse_target">
+        <a href="index.php" class="navbar-brand"><img src="Images/sheeklogo.jpg" alt=""></a>
+
+        <ul class="navbar-nav ml-auto">    
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="videos.php">Videos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+            </li>
+            <?php
+            $fname = $_SESSION['fname'];
+            echo '<li class="nav-item">
+                        <a class="nav-link" href="contact.php"> Hello, ' . $fname . '!</a>
+                        </li>'
+            ?>
+            <li class="nav-item">
+                <form action="loginsystem/logoutsubmit.php" method="POST">
+                    <button class="nav-link">Log Out</button>
+                </form>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+
+
+
+<!--
+<nav>
+<div class="logo">
+<a href="#"><img src="Images/sheeklogo.jpg" alt="logo"></a>
+</div>
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="videos.php">Videos</a></li>
+<li><a href="contact.php">Contact Us</a></li> 
+<li><a href="login.php">Login</a></li> 
+<li><a href="signup.php">Sign up</a></li>               
+</ul>   
+</nav>    
+-->
+
