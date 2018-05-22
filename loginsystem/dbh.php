493aaa49de9386abcc1448f671d7f2fc68bed7a5
Quang Tran <qtran1997@gmail.com>
2018-05-21 17:07:25 -0700

    hello

--- loginsystem/dbh.php
+++ loginsystem/dbh.php
@@ -1,6 +1,6 @@
 <?php
-//    $conn = mysqli_connect("localhost", "root", "1234", "AClickLoginData");
-    $conn = mysqli_connect("localhost", "root", "", "logintest");
+    $conn = mysqli_connect("http://ec2-54-183-8-67.us-west-1.compute.amazonaws.com", "root", "1234", "AClickLoginData");
+//    $conn = mysqli_connect("localhost", "root", "", "logintest");
     
     if (!$conn) {
         die("Connection Failed: ".mysqli_connect_error());
