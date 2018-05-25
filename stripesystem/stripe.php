8b9326a8907729b8cdc3829df43e3196c6ba214d
Quang Tran <qtran1997@gmail.com>
2018-05-25 14:38:11 -0700

    payment

--- stripesystem/stripe.php
+++ stripesystem/stripe.php
@@ -1,19 +1,22 @@
 <?php
-  require_once('./config.php');
+    require_once('./config.php');
 
-  $token  = $_POST['stripeToken'];
-  $email  = $_SESSION['email'];
+    $token  = $_POST['stripeToken'];
+    $email  = $_SESSION['email'];
 
-  $customer = \Stripe\Customer::create(array(
-      'email' => $email,
-      'source'  => $token
-  ));
+    $customer = \Stripe\Customer::create(array(
+        'email' => $email,
+        'source'  => $token
+    ));
 
-  $charge = \Stripe\Charge::create(array(
-      'customer' => $customer->id,
-      'amount'   => 50,
-      'currency' => 'usd'
-  ));
+    $charge = \Stripe\Charge::create(array(
+        'customer' => $customer->id,
+        'amount'   => 50,
+        'currency' => 'usd'
+    ));
 
-  echo '<h1>Successfully charged $0.50!</h1>';
+    
+    echo '<h1>Successfully charged $0.50!</h1>';
+    $sql = "UPDATE aclickerlogin SET pb= '1'";
+    $result = mysqli_query($conn, $sql);
 ?>
