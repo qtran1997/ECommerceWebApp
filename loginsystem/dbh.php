97672716b748c7d05f9db7356384b14bb780f70e
Quang Tran <qtran1997@gmail.com>
2018-05-25 11:13:43 -0700

    move to gitlab

--- loginsystem/dbh.php
+++ loginsystem/dbh.php
@@ -1,5 +1,6 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "1234", "AClickLoginData");
-//    $conn = mysqli_connect("localhost", "root", "", "logintest");
+//    $conn = mysqli_connect("localhost", "root", "1234", "AClickLoginData");
+    $conn = mysqli_connect("localhost", "root", "", "logintest");
+
 
 ?>
